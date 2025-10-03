961c674360f53d92909c3e084284ca587318bf23
fafakaj <mrainxxxm@gmail.com>
2025-10-03 12:38:32 +0300

    fixed any

--- Pylab3/Tests_Pylab3.py
+++ Pylab3/Tests_Pylab3.py
@@ -1,16 +1,34 @@
 import unittest
-from binary_tree import dict_bin_tree, custom_left, custom_right
+from binary_tree import dict_bin_tree
+
+
+def custom_left(x: int) -> int:
+    return x + 1
+# Пользовательская функция для значения левого корня
+
+def custom_right(x: int) -> int:
+    return x - 1
+# Пользовательская функция для значения правого корня
+
 
 class TestMath(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(dict_bin_tree(1), {0: 10})
+        self.assertEqual(dict_bin_tree(2, 111), {0: 111, 1: 334, 2: 332})
     def test_2(self):
-        self.assertEqual(dict_bin_tree(2), {0: 10, 1: 31, 2: 29})
-    def test_3(self):
         self.assertEqual(dict_bin_tree(3), {0: 10, 1: 31, 2: 29, 3: 94, 4: 92, 5: 88, 6: 86})
-    def test_4(self):
+    def test_3(self):
         self.assertEqual(dict_bin_tree(5), {0: 10, 1: 31, 2: 29, 3: 94, 4: 92, 5: 88, 6: 86, 7: 283,
         8: 281, 9: 277, 10: 275, 11: 265, 12: 263, 13: 259, 14: 257, 15: 850, 16: 848, 17: 844, 18: 842,
         19: 832, 20: 830, 21: 826, 22: 824, 23: 796, 24: 794, 25: 790, 26: 788, 27: 778, 28: 776, 29: 772, 30: 770})
+    def test_4(self):
+        self.assertEqual(dict_bin_tree(3, l_b = custom_left, r_b = custom_right),
+                         {0: 10, 1: 11, 2: 9, 3: 12, 4: 10, 5: 10, 6: 8})
     def test_5(self):
-        self.assertEqual(dict_bin_tree(3, l_b=custom_left, r_b=custom_right), {0: 10, 1: 11, 2: 9, 3: 12, 4: 10, 5: 10, 6: 8})
+        self.assertEqual(dict_bin_tree(3, 55, l_b = custom_left),
+                         {0: 55, 1: 56, 2: 164, 3: 57, 4: 167, 5: 165, 6: 491})
+    def test_6(self):
+        self.assertEqual(dict_bin_tree(2, 111, r_b = custom_right),
+                         {0: 111, 1: 334, 2: 110})
+    def test_7(self):
+        self.assertEqual(dict_bin_tree(3, 15, l_b = custom_left, r_b = custom_right),
+                         {0: 15, 1: 16, 2: 14, 3: 17, 4: 15, 5: 15, 6: 13})
--- Pylab3/binary_tree.py
+++ Pylab3/binary_tree.py
@@ -1,65 +1,60 @@
-def dict_bin_tree(height, l_b = None, r_b = None):
-    """Функция рекурсивно генирирующая полное бинарное дерево с заданной высотой для unittest."""
-    dictionary: dict[int, int] = {}
-    root_value = 10
+def dict_bin_tree(height: int, root_value = 10, l_b = None, r_b = None) -> dict:
+    """Функция рекурсивно генирирующая полное бинарное дерево с заданной высотой для unittest.
 
+    height: int - высота бинарного дерева
+    root_value: int - изначальное значение для корня с индексом 0
+    l_b - функция для левого корня
+    r_b - функfrom Tests_Pylab3 import custom_right, custom_leftиця для правого корня
+    """
+
+    def alexander(root_value: int) -> int:
+        """Вычисляет значение левого корня по формуле: (3 * x + 1).
+
+            root_value - значение корня с индексом 0
+            """
+        return root_value * 3 + 1
+
+    def lesnitskiy(root_value: int) -> int:
+        """Вычисляет значение правого корня по формуле: (3 * x - 1).
+
+            root_value - значение корня с индексом 0
+            """
+        return 3 * root_value - 1
 
     def bin_tree(height, root = 0, l_b = None, r_b = None, h_actually = 1):
         """ Функция рекурсивно генирирующая полное бинарное дерево с заданной высотой.
 
-        height - высота бинарного дерева
-        root - номер корня
-        l_b - функция для левого потомка
-        r_b - функиця для правого потомка
-        h_actually - актуальная высота какого-то корня
+        height: int - высота бинарного дерева
+        root: int - номер корня
+        l_b - функция для левого корня
+        r_b - функиця для правого корня
+        h_actually: int - актуальная высота какого-то корня
         """
+
         if h_actually < height:
             actually_value = dictionary[root]
 
-            # Для левого потомка
+            # Для левого корня
             left_value = l_b(actually_value)
             left_index = 2 * root + 1
             dictionary[left_index] = left_value
             bin_tree(height, left_index, l_b, r_b, h_actually + 1)
 
-            # Для правого потомка
+            # Для правого корня
             right_value = r_b(actually_value)
             right_index = 2 * root + 2
             dictionary[right_index] = right_value
             bin_tree(height, right_index, l_b, r_b, h_actually + 1)
 
-
-    def alexander(root_value: int) -> int:
-        """Вычисляет значение левого потомка по формуле: (3 * x + 1).
-
-            root_value - значение родительского корня
-            """
-        return root_value * 3 + 1
-
-
-    def lesnitskiy(root_value: int) -> int:
-        """Вычисляет значение правого потомка по формуле: (3 * x - 1).
-
-            root_value - значение родительского корня
-            """
-        return 3 * root_value - 1
-
-
     if l_b is None:
         l_b = alexander
     if r_b is None:
         r_b = lesnitskiy
+
+    dictionary: dict[int, int] = dict()
     dictionary[0] = root_value
     bin_tree(height, l_b = l_b, r_b = r_b)
     sorted_dict = sorted(dictionary.items())
     dictionary.clear()
     dictionary.update(sorted_dict)
     return dictionary
-
-def custom_left(x: int) -> int:
-    return x + 1
-# Пользовательская функция для значения левого корня
-
-def custom_right(x: int) -> int:
-    return x - 1
-# Пользовательская функция для значения правого корня
